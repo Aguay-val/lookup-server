02845fc42153bbc39203e3e73d05db6f404d7ed2
Lionel MORIN <lionel.morin@ac-dijon.fr>
2021-11-15 15:37:37 +0100

    Return formatted users

--- poc_flask_pymongo_restful.py
+++ poc_flask_pymongo_restful.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from flask_restful import Resource, Api
+import datetime
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:3001/meteor"
@@ -20,7 +21,7 @@ class Users(Resource):
         searchstring = request.args.get('search')
         if searchstring:
             datas = mongo.db.users.find({'$or':[
-                { 'emails': {'$elemMatch': {'address': { '$regex': f".*{searchstring}.*", '$options': 'i' }}}},
+                {'emails': {'$elemMatch': {'address': { '$regex': f".*{searchstring}.*", '$options': 'i' }}}},
                 {'username': { '$regex': f".*{searchstring}.*", '$options': 'i' }},
                 {'lastName': { '$regex': f".*{searchstring}.*", '$options': 'i' }},
                 {'firstName': { '$regex': f".*{searchstring}.*", '$options': 'i' }}
@@ -28,13 +29,41 @@ class Users(Resource):
             )
         else:
             datas = mongo.db.users.find()
-        return jsonify(list(datas))
+        return self.formatUsersResult(datas)
+    
+    def formatUsersResult(self, datas):
+        resSingleUser = {
+        'message' : {
+            'data' : {
+            'federationId' : '',
+            'name' : '',
+            'email' : '',
+            'address' : '',
+            'website' : '',
+            'twitter' : '',
+            'phone' : ''
+            },
+            'type' : 'lookupserver-apps',
+            'timestamp' : 0,
+            'signer' : 'yo'
+        },
+            'signature' : ''
+        }
+        res = []
+        for u in list(datas):
+            resDict = resSingleUser.copy()
+            resDict['message']['data']['federationId'] = f"{u['username']}@{u['nclocator']}"
+            resDict['message']['data']['name'] = f"{u['firstName']}@{u['lastName']}"
+            resDict['message']['data']['email'] = f"{u['emails'][0]['address']}"
+            resDict['message']['timestamp'] = int(datetime.datetime.now().timestamp())
+            res.append(resDict)
+        return jsonify(res)
         
         
 class User(Resource):
     def get(self, name):
         datas = mongo.db.users.find({'username':name})
-        return jsonify(list(datas))
+        return self.formatUsersResult(datas)
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(Users, '/users')
