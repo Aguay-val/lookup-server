3e4ced77806801406cc5bb2fcf45bd503137489a
Daniel Dehennin <daniel.dehennin@ac-dijon.fr>
2021-12-20 08:40:11 +0100

    style(python): force black formatting of code
    This will make possible to add a CI job to enforce formatting.

--- init.py
+++ init.py
@@ -6,10 +6,10 @@ from flask_pymongo import PyMongo
 from os import environ
 
 app = Flask(__name__)
-print("MONGO_URI = ", environ['MONGO_URI'])
-app.config["MONGO_URI"] = environ['MONGO_URI']
+print("MONGO_URI = ", environ["MONGO_URI"])
+app.config["MONGO_URI"] = environ["MONGO_URI"]
 try:
-    app.config["MONGO_LIMIT"] = int(environ['MONGO_LIMIT'])
+    app.config["MONGO_LIMIT"] = int(environ["MONGO_LIMIT"])
 except:
     app.config["MONGO_LIMIT"] = 10
 
